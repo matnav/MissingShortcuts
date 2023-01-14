ac25c73b786507aa13982d42548fb35ea1845f34
matnav <xmatnavx@gmail.com>
2023-01-14 09:13:40 -0600

    Update Get-Shortcuts.ps1

--- Get-Shortcuts.ps1
+++ Get-Shortcuts.ps1
@@ -1,4 +1,4 @@
-<#
+#
     .SYNOPSIS
     This script is used to get all the shortcuts on the desktop and exports them to a CSV file, with the option to exclude certain paths.
     
@@ -21,7 +21,7 @@
     Show the output of the script on console or not
     
     .EXAMPLE
-    .\Get-DesktopShortcuts.ps1 -ExcludePaths "Accessibility", "Accessories", "Administrative" -FilePath "C:\matnav\shortcuts.csv" -SearchPath "C:\ProgramData\Microsoft\Windows\Start Menu" -ShowOutput $true
+    .\Get-Shortcuts.ps1 -ExcludePaths "Accessibility", "Accessories", "Administrative" -FilePath "C:\matnav\shortcuts.csv" -SearchPath "C:\ProgramData\Microsoft\Windows\Start Menu" -ShowOutput $true
     This will run the script and export the shortcuts to a CSV file, excluding any paths that contain the specified strings, looking for shortcuts in "C:\ProgramData\Microsoft\Windows\Start Menu" and Showing the output on console.
     
     .NOTES
@@ -31,6 +31,31 @@
 
 param (
     [string[]]$ExcludePaths = @(),
-    [string]$FilePath = "c:\matnav\shortcuts.csv",
-    [string]$SearchPath = "C:\ProgramData\Microsoft\Windows\Start Menu",
-    [bool]$
+    [string]$FilePath = "c:\matnav\shortcuts.csv"
+)
+
+function Get-Shortcuts {
+    # Define an array to store the shortcut objects
+    $Shortcuts = @()
+
+    # Get all the shortcuts in the Start Menu folder
+    $AllShortcuts = Get-ChildItem -Recurse $SearchPath -Include *.lnk
+
+    # Create a WScript.Shell object
+    $Shell = New-Object -ComObject WScript.Shell
+
+    # Loop through each shortcut
+    foreach ($Shortcut in $AllShortcuts) {
+        # Check if the shortcut path contains any of the excluded strings
+        if ($ExcludePaths -notcontains ($Shortcut.DirectoryName | Select-String -SimpleMatch -Quiet)) {
+            # Create a custom object to store the shortcut properties
+            $Properties = @{
+                ShortcutName     = $Shortcut.Name
+                ShortcutFull     = $Shortcut.FullName
+                ShortcutPath     = $shortcut.DirectoryName
+                Target           = $Shell.CreateShortcut($Shortcut).targetpath
+                WorkingDirectory = $Shell.CreateShortcut($Shortcut).WorkingDirectory
+            }
+            $Shortcuts += New-Object PSObject -Property $Properties
+            if($ShowOutput){
+                Write-Output "ShortcutName: $($Properties.ShortcutName) ShortcutFull: $($Properties.ShortcutFull) ShortcutPath: $($Properties.ShortcutPath) Target: $
