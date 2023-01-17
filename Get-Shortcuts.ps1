ac5686f1771cb0518c20103374dd3c8bd5d33a09
matnav <xmatnavx@gmail.com>
2023-01-17 14:54:06 -0600

    Update Get-Shortcuts.ps1

--- Get-Shortcuts.ps1
+++ Get-Shortcuts.ps1
@@ -1,40 +1,10 @@
-<#
-    .SYNOPSIS
-    This script is used to get all the shortcuts on the desktop and exports them to a CSV file, with the option to exclude certain paths.
-    
-    .DESCRIPTION
-    The script uses the Get-ChildItem cmdlet to recursively search the Start Menu folder for all
-    shortcuts, and then uses the WScript.Shell COM object to retrieve properties of the shortcuts.
-    The properties are then stored in a custom object, which is added to an array. The array is then 
-    exported to a CSV file.
-    
-    .PARAMETER ExcludePaths
-    The paths that you want to exclude when searching for shortcuts. These can be partial paths, and will match any path that contains the specified string.
-    
-    .PARAMETER FilePath
-    The path where the CSV file will be exported to.
-    
-    .PARAMETER SearchPath
-    The path where the script looks for the shortcuts
-    
-    .PARAMETER ShowOutput
-    Show the output of the script on console or not
-    
-    .EXAMPLE
-    .\Get-Shortcuts.ps1 -ExcludePaths "Accessibility", "Accessories", "Administrative" -FilePath "C:\matnav\shortcuts.csv" -SearchPath "C:\ProgramData\Microsoft\Windows\Start Menu" -ShowOutput $true
-    This will run the script and export the shortcuts to a CSV file, excluding any paths that contain the specified strings, looking for shortcuts in "C:\ProgramData\Microsoft\Windows\Start Menu" and Showing the output on console.
-    
-    .NOTES
-    Author: Matthew Navarette
-    Date Created: January 14, 2023
-#>
-
 param (
     [string[]]$ExcludePaths = @(),
-    [string]$FilePath = "c:\matnav\shortcuts.csv",
+    [string]$FilePath = "c:\matnav\shortcuts-uptodate.csv",
     [string]$SearchPath = "C:\ProgramData\Microsoft\Windows\Start Menu",
     [bool]$ShowOutput = $false
 )
+
 
 function Get-Shortcuts {
     # Define an array to store the shortcut objects
@@ -48,30 +18,50 @@ function Get-Shortcuts {
 
     # Loop through each shortcut
     foreach ($Shortcut in $AllShortcuts) {
-        # Check if the shortcut path contains any of the excluded strings
-        if ($ExcludePaths -notcontains ($Shortcut.DirectoryName | Select-String -SimpleMatch -Quiet)) {
-# Create a custom object to store the shortcut properties
-$Properties = @{
-ShortcutName = $Shortcut.Name
-ShortcutFull = $Shortcut.FullName
-ShortcutPath = $shortcut.DirectoryName
-Target = $Shell.CreateShortcut($Shortcut).TargetPath
-Arguments = $Shell.CreateShortcut($Shortcut).Arguments
-WorkingDirectory = $Shell.CreateShortcut($Shortcut).WorkingDirectory
-}
-$Shortcuts += New-Object PSObject -Property $Properties
-if($ShowOutput){
-Write-Output "ShortcutName: $($Properties.ShortcutName) ShortcutFull: $($Properties.ShortcutFull) ShortcutPath: $($Properties.ShortcutPath) Target: $($Properties.Target) Arguments: $($Properties.Arguments) WorkingDirectory: $($Properties.WorkingDirectory)"
-}
-}
-}
-# Release the COM object
-[Runtime.InteropServices.Marshal]::ReleaseComObject($Shell) | Out-Null
-# Export the array to a CSV file
-$Shortcuts | ConvertTo-Csv -NoTypeInformation | Out-File $FilePath -Force
-
-# Return the number of shortcuts found
-Write-Output "Found $($Shortcuts.Count) shortcuts"
+        if ($ExcludePaths.Length -eq 0) {
+            # Create a custom object to store the shortcut properties
+            $Properties = @{
+                ShortcutName     = $Shortcut.Name
+                ShortcutFull     = $Shortcut.FullName
+                ShortcutPath     = $shortcut.DirectoryName
+                Target           = $Shell.CreateShortcut($Shortcut).TargetPath
+                Arguments        = $Shell.CreateShortcut($Shortcut).Arguments
+                WorkingDirectory = $Shell.CreateShortcut($Shortcut).WorkingDirectory
+            }
+            $Shortcuts += New-Object PSObject -Property $Properties
+            if($ShowOutput){
+                Write-Output "ShortcutName: $($Properties.ShortcutName) ShortcutFull: $($Properties.ShortcutFull) ShortcutPath: $($Properties.ShortcutPath) Target: $($Properties.Target) Arguments: $($Properties.Arguments) WorkingDirectory: $($Properties.WorkingDirectory)"
+            }
+        }
+        else {
+            # Check if the shortcut path contains any of the excluded strings
+            if ($ExcludePaths -notcontains ($Shortcut.DirectoryName | Select-String -SimpleMatch -Quiet)) {
+                # Create a custom object to store the shortcut properties
+                $Properties = @{
+                    ShortcutName     = $Shortcut.Name
+                    ShortcutFull     = $Shortcut.FullName
+                    ShortcutPath     = $shortcut.DirectoryName
+                    Target           = $Shell.CreateShortcut($Shortcut).TargetPath
+                    Arguments        = $Shell.CreateShortcut($Shortcut).Arguments
+                    WorkingDirectory = $Shell.CreateShortcut($Shortcut).WorkingDirectory
+                }
+                $Shortcuts += New-Object PSObject -Property $Properties
+                if($ShowOutput){
+                    Write-Output "ShortcutName: $($Properties.ShortcutName) ShortcutFull: $($Properties.ShortcutFull) ShortcutPath: $($Properties.ShortcutPath) Target: $($Properties.Target) Arguments: $($Properties.Arguments) WorkingDirectory: $($Properties.WorkingDirectory)"
+                }
+            }
+        }
+    }
+    # Release the COM object
+    [Runtime.InteropServices.Marshal]::ReleaseComObject($Shell) | Out-Null
+        if (!(Test-Path -Path $FilePath)) {
+        # Create the parent directory if it does not exist
+        New-Item -ItemType Directory -Path (Split-Path $FilePath -Parent)  -ErrorAction SilentlyContinue| Out-Null
+    }
+    # Export the array to a CSV file
+    $Shortcuts | Export-Csv -Path $FilePath -NoTypeInformation -Force
+    # Return the number of shortcuts found
+    Write-Output "Found $($Shortcuts.Count) shortcuts"
 }
 
 Get-Shortcuts
